e6bd2d0ca3a532c4d16c3d36b57f8aabee4aafae
pindukurisairam <sairamsai524@gmail.com>
2023-08-15 13:08:18 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import UserProfile from './components/UserProfile'
+
+import './App.css'
+
+const userDetailsList = [
+  {
+    uniqueNo: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'sairam',
+    role: 'software developer',
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'pallvai',
+    role: 'software developer',
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'lucy',
+    role: 'software developer',
+  },
+  {
+    uniqueNo: 4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
+    name: 'santoosh',
+    role: 'software developer',
+  },
+]
+const App = () => (
+  <div className="list-container">
+    <h1 className="title">User List</h1>
+    <ul>
+      {userDetailsList.map(eachItem => (
+        <UserProfile userDetails={eachItem} key={eachItem.uniqueNo} />
+      ))}
+    </ul>
+  </div>
+)
+
+export default App
